27cb2e579b47e746cc0f437bd6aac54f342a9189
Ethan Graham <ethangraham43@gmail.com>
2022-10-24 14:34:39 -0400

    finished user migration

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, :session_token, presense: true, uniqueness: true
+    validates :session_token, uniqueness: {scope: :}
+    before_validation :ensure_session_token
+
+    private
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
