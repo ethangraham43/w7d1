55e3288f7c06cff10e51d310246f7765a3a49170
“sajeremy” <9jsantiago4@gmail.com>
2022-10-24 16:35:54 -0400

    new view for sesssions

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/user_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/user_controller.rb
@@ -2,18 +2,22 @@ class UserController < ApplicationController
     def new
     end
 
-    def show
-        @user = User.find_by(id: params[:id])
-
-    end
+    # def show
+    #     @user = User.find_by(id: params[:id])
+    # end
 
     def create
-        @user = User.new(params[:username, :password])
+        @user = User.new(user_params)
         if @user.save
-            # login(@user)
+            login(@user)
             redirect_to cats_url
         else
             render json: @user.errors.full_messages, status: 422
         end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
