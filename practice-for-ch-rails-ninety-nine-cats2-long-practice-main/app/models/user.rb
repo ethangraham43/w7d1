53ffe196a251bbd2d80628d8bcbdf6da6e19e4dd
“sajeremy” <9jsantiago4@gmail.com>
2022-10-24 15:02:06 -0400

    review code

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -10,9 +10,34 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-    validates :username, :session_token, presense: true, uniqueness: true
-    validates :session_token, uniqueness: {scope: :}
+    validates :username, :session_token, presence: true, uniqueness: true
     before_validation :ensure_session_token
+    attr_reader :password
+
+    def self.find_by_credentials(username,password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+    
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
 
     private
     def generate_unique_session_token
